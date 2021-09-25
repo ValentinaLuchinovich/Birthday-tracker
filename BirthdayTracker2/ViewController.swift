e4a6b83d7ce731fadc014173af4e0037f6e63ab7
Valentina Luchinovich <valinkontakt11@bk.ru>
2021-09-25 15:07:10 +0300

    Initial Commit

---
+++ BirthdayTracker2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BirthdayTracker2
+//
+//  Created by Валентина Лучинович on 25.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
